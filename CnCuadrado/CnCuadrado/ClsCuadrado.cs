9d15faa146d38b6796985706fa215dab24b4f6ca
simoncortes0814 <126583621+simoncortes0814@users.noreply.github.com>
2023-03-28 16:52:26 -0500

    Add files via upload

---
+++ CnCuadrado/CnCuadrado/ClsCuadrado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuadrado
+{
+    class ClsCuadrado
+    {
+        public int lado;
+        public int total;
+
+        public ClsCuadrado()
+        {
+
+            lado = 0;
+            total = 0;
+
+        }
+
+        public void PerimetroCuadrado ()
+        {
+            total = lado * 4;
+            Console.WriteLine("El perimetro equivale a: " + total + "Cm");
+        }
+
+        public void SuperficieCuadrado ()
+        {
+            total = lado * lado;
+            Console.WriteLine("La superficie del cuadrado es igual a: " + total + "Cm cuadrados");
+        }
+
+        
+    }
+}
---
+++ CnCuadrado/CnCuadrado/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ClsCuadrado Cuadrado = new ClsCuadrado();
+
+            Console.WriteLine("Ingrese por favor el lado del cuadrado para así sacar la superficie y el perimetro ");
+            Cuadrado.Lados = int.Parse(Console.ReadLine());
+
+            //Llamamos los metodos para las formulas
+
+            Cuadrado.PerimetroFormula();
+
+            Cuadrado.SuperficieFormula();
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
