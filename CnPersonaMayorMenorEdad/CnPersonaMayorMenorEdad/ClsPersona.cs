ed49136f7a5401ec38646447eb711746222575d1
simoncortes0814 <126583621+simoncortes0814@users.noreply.github.com>
2023-03-28 16:49:40 -0500

    Add files via upload

---
+++ CnPersonaMayorMenorEdad/CnPersonaMayorMenorEdad/ClsPersona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnPersonaMayorMenorEdad
+{
+    class ClsPersona
+    {
+        public string nombre, apellido, correo;
+        public int edad;
+        public ClsPersona()
+        {
+            nombre = "";
+            correo = "";
+            apellido = "";
+            edad = 0;
+
+        }
+
+        public void MayorMenor()
+        {   
+            
+            if (edad >= 18)
+            {
+                Console.WriteLine( nombre + " Termine su registro por favor");
+                Console.WriteLine("Por favor ingrese su apellido");
+                apellido = Console.ReadLine();
+                Console.WriteLine("Por favor ingrese su correo electronico");
+                correo = Console.ReadLine();
+                Console.WriteLine("Se ha registrado con exito");
+            }
+            else if (edad < 18)
+            {
+                Console.WriteLine("Usted es menor de edad, no puede terminar el registro");
+            }
+
+        }
+    }
+
+}
---
+++ CnPersonaMayorMenorEdad/CnPersonaMayorMenorEdad/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnPersonaMayorMenorEdad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsPersona PersonaDatos = new ClsPersona();
+
+            Console.WriteLine("Por favor ingrese su nombre");
+            PersonaDatos.nombre = Console.ReadLine();
+            Console.WriteLine("Por favor ingrese su edad");
+            PersonaDatos.edad = int.Parse(Console.ReadLine());
+            PersonaDatos.MayorMenor();
+
+            Console.ReadKey();
+
+        }
+    }
+}
