e9759878ed1c1c3e6787d816d3dc218720cdc0c8
simoncortes0814 <126583621+simoncortes0814@users.noreply.github.com>
2023-03-28 16:53:10 -0500

    Add files via upload

---
+++ CnMultiplicación/Multiplicación/ClsMultiplicar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicación
+{
+    class ClsMultiplicar
+    {
+        public int Numero;
+        public int i;
+        public int total;
+        public string Salir;
+        public ClsMultiplicar()
+        {
+            Numero = 0;
+            i = 0;
+            total = 0;
+        }
+
+        public void Multiplicación()
+        {
+            Console.WriteLine("Ingrese el número que desee multiplicar");
+            Numero = int.Parse(Console.ReadLine());
+            while (i<13)
+            {
+                total = Numero * i;
+                Console.WriteLine(Numero + "*" + i + "=" + total);
+                i += 1;
+            }
+            Console.WriteLine("Para finalizar el ejercicio escriba -1");
+            Salir = Console.ReadLine();
+            if (Salir == "-1")
+            {
+                Environment.Exit(0);
+            }
+            
+            
+            
+        }
+
+    }
+}
---
+++ CnMultiplicación/Multiplicación/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Confeccionar una clase que permita ingresar valores enteros por teclado y nos muestre la tabla de multiplicar de dicho valor. Finalizar el programa al ingresar el -1.
+            ClsMultiplicar Multiplicacion = new ClsMultiplicar();
+
+            Multiplicacion.Multiplicación();
+            Console.ReadKey();
+
+        }
+    }
+}
