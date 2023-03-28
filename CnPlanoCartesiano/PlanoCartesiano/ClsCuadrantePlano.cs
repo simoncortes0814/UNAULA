bb647b030908ac9c91eb874756fc208f5967c70e
simoncortes0814 <126583621+simoncortes0814@users.noreply.github.com>
2023-03-28 16:55:14 -0500

    Add files via upload

---
+++ CnPlanoCartesiano/PlanoCartesiano/ClsCuadrantePlano.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanoCartesiano
+{
+    class ClsCuadrantePlano
+    {
+       public int x, y;
+
+        public ClsCuadrantePlano ()
+        {
+            x = 0;
+            y = 0;
+
+        }
+        
+        public void Cuadrantes ()
+        {
+            Console.WriteLine("Ingrese del punto en x : ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese del punto en y : ");
+            y = int.Parse(Console.ReadLine());
+
+            if (x > 0 & y > 0)
+            {
+                Console.WriteLine("(" + x + "," + y + ")");
+                Console.WriteLine("Los siguientes puntos son positivos, por lo que se encuentran en el cuadrante número 1");
+
+            }
+            else if (x < 0 & y > 0)
+            {
+                Console.WriteLine("(" + x + "," + y + ")");
+                Console.WriteLine("El primer punto es negativo y el segundo positivo, por lo que se encuentran en el cuadrante número 2");
+
+            }
+            else if (x < 0 & y < 0)
+            {
+                Console.WriteLine("(" + x + "," + y + ")");
+                Console.WriteLine("Los dos puntos son negativos, por lo que se encuentran en el cuadrante número 3");
+
+            }
+            else if (x > 0 & y < 0)
+            {
+
+                Console.WriteLine("(" + x + "," + y + ")");
+                Console.WriteLine("El primer punto es positivo y el segundo es negativo, por lo que se encuentran en el cuadrante número 4");
+
+            }
+        }
+    }
+}
---
+++ CnPlanoCartesiano/PlanoCartesiano/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanoCartesiano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsCuadrantePlano Plano = new ClsCuadrantePlano();
+
+            Plano.Cuadrantes();
+            Console.ReadKey();
+        }
+    }
+}
