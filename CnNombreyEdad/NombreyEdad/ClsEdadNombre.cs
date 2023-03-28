cb5e0f2ff4f68515c005c39dd164b4e5af38805c
simoncortes0814 <126583621+simoncortes0814@users.noreply.github.com>
2023-03-28 16:56:04 -0500

    Add files via upload

---
+++ CnNombreyEdad/NombreyEdad/ClsEdadNombre.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NombreyEdad
+{
+    class ClsEdadNombre
+    {
+        
+        private string nombre;
+        private int edad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+
+        //Metodo Constructor 
+
+        public ClsEdadNombre()
+        {
+
+            Nombre = "";
+            Edad = 0;
+
+        }
+            
+
+    }
+}
---
+++ CnNombreyEdad/NombreyEdad/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NombreyEdad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsEdadNombre Persona = new ClsEdadNombre();
+            Console.WriteLine("Por favor ingrese su edad principalmente");
+            Persona.Edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese ahora su nombre para subirlo al sistema");
+            Persona.Nombre = Console.ReadLine();
+
+            
+            if (Persona.Edad < 18)
+            {
+                
+                Console.WriteLine(Persona.Nombre + " usted tiene: " + Persona.Edad + " años " + " por lo que usted es menor de edad, podra ser registrado en el sistema cuando cumpla los 18 años" );
+            }
+            else if (Persona.Edad >= 18)
+            {
+
+                Console.WriteLine(Persona.Nombre + " usted tiene: " + Persona.Edad + " años" + " usted ha sido registrado en el sistema con exito");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
