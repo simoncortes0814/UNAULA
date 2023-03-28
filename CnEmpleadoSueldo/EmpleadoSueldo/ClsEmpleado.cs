425dc0a5e2aff010269fb533c85b88462f0afc4f
simoncortes0814 <126583621+simoncortes0814@users.noreply.github.com>
2023-03-28 16:56:47 -0500

    Add files via upload

---
+++ CnEmpleadoSueldo/EmpleadoSueldo/ClsEmpleado.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpleadoSueldo
+{
+    class ClsEmpleado
+    {
+        private string nombre;
+        private int sueldo;
+        private string apellido;
+       
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Sueldo { get => sueldo; set => sueldo = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+
+        public ClsEmpleado()
+        {
+            Nombre = "";
+            Sueldo = 0;
+            Apellido = "";
+       
+        }
+
+        public void CargarDatos()
+        {
+            Console.WriteLine("Señor Usuario, por favor ingrese su nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Señor Usuario, por favor ingrese su apellido");
+            Apellido = Console.ReadLine();
+            Console.WriteLine("Señor Usuario, por favor ingrese su sueldo");
+            Sueldo = int.Parse(Console.ReadLine());
+         
+
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Señor " + Nombre + " " + Apellido);
+            Console.WriteLine("Su sueldo es de: " + Sueldo + " Dolares" );
+     
+           
+        }
+        
+        
+
+        public void Impuesto()
+        {
+            if (Sueldo >= 3000)
+            {
+                Console.WriteLine("Señor " + Nombre + " usted debe de pagar impuesto.");
+            }
+            else
+            {
+                Console.WriteLine("Señor " + Nombre + " usted no debe de pagar impuesto.");
+            }
+
+        }
+
+        
+
+
+        
+
+    }
+}
---
+++ CnEmpleadoSueldo/EmpleadoSueldo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpleadoSueldo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsEmpleado Empleado = new ClsEmpleado();
+
+            Empleado.CargarDatos();
+            Empleado.Imprimir();
+            Empleado.Impuesto();
+
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
